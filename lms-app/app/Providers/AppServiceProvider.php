609db1721a37d3c7e774bc9a2a16a846af60ddac
congvt <congvtc02@gmail.com>
2026-07-28 00:26:50 +0700

    fix: Force HTTPS scheme for assets on production

--- lms-app/app/Providers/AppServiceProvider.php
+++ lms-app/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Repositories\User\IUserRepository;
 use App\Repositories\User\UserRepository;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Đăng ký binding repository người dùng
         $this->app->bind(IUserRepository::class, UserRepository::class);
     }
 
@@ -22,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Ép buộc toàn bộ đường dẫn link asset/css/js dùng HTTPS khi trên Production
+        if ($this->app->environment('production') || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            URL::forceScheme('https');
+        }
     }
 }
